cc3179bf3dc4f6035348124c3ee7ab45f7f10b7c
Ronny <ronnybet41@gmail.com>
2022-08-23 15:43:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let text = document.getElementById("text")
+
+document.addEventListener( "DOMContentLoaded",updateDOM);
+  
+  
+  function updateDOM() {
+   
+      text.innerHTML = "This is really cool!";
+  }
